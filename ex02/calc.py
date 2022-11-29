2a57ac92086a9a90aa880d93ead0171bafc512e2
mareto <c0a21055d9@edu.teu.ac.jp>
2022-11-29 14:59:46 +0900

    練習問題2

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,7 +2,21 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
+
+
 #ウィンドウ作成
 root = tk.Tk()
 root.geometry("300x500")
+
+#数字ボタン作成
+r, c = 1, 0
+for i in range(9, -1, -1):
+    button = tk.Button(root, text=f"{i}", width=4, height=2,font=("", "30"))
+    button.grid(row= r , column = c)
+    c += 1
+
+    if c % 3 == 0:
+        r += 1
+        c = 0
+
 root.mainloop()
