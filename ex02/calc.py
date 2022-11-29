2a0b323f63271f5052fd84008184264df3bc3fc3
mareto <c0a21055d9@edu.teu.ac.jp>
2022-11-29 16:24:38 +0900

    練習問題7

--- ex02/calc.py
+++ ex02/calc.py
@@ -13,6 +13,17 @@ def number_click(event):
     btn = event.widget
     num = btn["text"]
     entry.insert(tk.END,num)
+    
+def plus_click(event):
+    btn = event.widget
+    num = btn["text"]
+    entry.insert(tk.END, "+")
+#＝をクリックした場合
+def equal_click(event):
+    eqn = entry.get()
+    re = eval(eqn)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, re)
 
 #ウィンドウ作成
 root = tk.Tk()
@@ -28,15 +39,20 @@ for i in range(9, -1, -1):
     button = tk.Button(root, text=f"{i}", width=4, height=2,font=("", "30"))
     button.grid(row= r , column = c)
     button.bind("<1>", number_click)
-    
     c += 1
 
     if c % 3 == 0:
         r += 1
         c = 0
-button = tk.Button(root, text="+", width=4, height=2, font=("", "30"))
-button.grid(row= 4 , column = 1)
+
 button = tk.Button(root, text="=", width=4, height=2, font=("", "30"))
 button.grid(row= 4 , column = 2)
+button.bind("<1>", equal_click)
+
+button = tk.Button(root, text="+", width=4, height=2, font=("", "30"))
+button.grid(row= 4 , column = 1)
+button.bind("<1>", plus_click)
+
+
 
 root.mainloop()
