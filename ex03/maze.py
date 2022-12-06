aec8eaffbabab657eb88f53b26df4f2ad4f9b62b
mareto <c0a21055d9@edu.teu.ac.jp>
2022-12-06 14:34:59 +0900

    練習2

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     root.geometry("1500x900")
-    root.mainloop()
 
+    #練習2
+    canvas = tk.Canvas(root, width=1500, height=900, bg = "black")
+    canvas.pack()
+    root.mainloop()
